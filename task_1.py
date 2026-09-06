c98e1f7b531674fdeec603128cd079f0606deb63
shibkov <shibkov0306@gmail.com>
2026-09-06 19:15:48 +0400

    first commit

---
+++ task_1.py
@@ -0,0 +1,19 @@
+
+def get_system_info():
+    """
+    Эта функция должна вернуть словарь с информацией о вашей "системе".
+    """
+    # TODO: Заполните словарь вашими реальными данными
+    system_info = {
+        "student_name": "Шибков Александр Алексеевич",
+        "academic_group": "ИВТИИбд-11",
+        "github_link": "https://github.com/Leverbust"
+    }
+    return system_info
+
+# Вывод информации для проверки
+if __name__ == "__main__":
+    info = get_system_info()
+    print("Информация о системе:")
+    for key, value in info.items():
+        print(f"- {key}: {value}")
